0babfa9809aca34856c27c8d292db02e3491f309
fariz0020 <fariz0020@students.amikom.ac.id>
2017-05-12 18:19:27 +0700

    1st commit

---
+++ app/src/main/java/id/skripsi/fariz/mobilevisonapp/Config.java
@@ -0,0 +1,11 @@
+package id.skripsi.fariz.mobilevisonapp;
+
+/**
+ * Created by didi on 5/12/2017.
+ */
+
+public class Config {
+
+    public static String URL_API= "http://rumah2in1.com/skripsifariz/index.php/api/";
+    public static String API_TRANSLATE= "translate/terjemah/";
+}
---
+++ app/src/main/java/id/skripsi/fariz/mobilevisonapp/MainActivity.java
@@ -0,0 +1,180 @@
+package id.skripsi.fariz.mobilevisonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView cameraView;
+    private TextView textBlockContent;
+    private CameraSource cameraSource;
+
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (SurfaceView) findViewById(R.id.surface_view);
+        textBlockContent = (TextView) findViewById(R.id.text_value);
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Detector dependencies are not yet available.");
+        }
+
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1280, 1024)
+                .setRequestedFps(2.0f)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    //noinspection MissingPermission
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                Log.d("Main", "receiveDetections");
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if (items.size() != 0) {
+                    textBlockContent.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final StringBuilder value = new StringBuilder();
+                            final String[] str = new String[1];
+                            for (int i = 0; i < items.size(); ++i) {
+                                final TextBlock item = items.valueAt(i);
+                                Log.d("TAG textblock", item.getValue());
+                                getTranslate(item.getValue(), new VolleyCallback(){
+                                    @Override
+                                    public void onSuccess(String result){
+                                        Log.d("TAG-result", result);
+                                        str[0] = result;
+                                    }
+                                });
+                                value.append(str[0]);
+                                value.append("\n");
+                                Log.d("TAG textblock-append", str[0]);
+
+                            }
+                            //update text block content to TextView
+                            Log.d("TAG textblock-after ", value.toString());
+                            textBlockContent.setText(value.toString());
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraSource.release();
+    }
+
+    private void getTranslate(final String string, final VolleyCallback callback) {
+        final String url = String.format(Config.URL_API+Config.API_TRANSLATE+"?text=%1$s", string);
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        Log.d(TAG + "url", url);
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject api = (JSONObject) response
+                                        .get(i);
+
+                                String terjemah = api.getString("terjemah");
+
+                                callback.onSuccess(terjemah);
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            callback.onSuccess(string);
+                            e.printStackTrace();
+                            Toast.makeText(AppController.getInstance().getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                callback.onSuccess(string);
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(AppController.getInstance().getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+    public interface VolleyCallback{
+        void onSuccess(String result);
+    }
+
+}
